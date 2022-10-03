668eff0b5510515f4407afd9f083b5d9cc6e0427
neftalicruzsosa <neftalicruzsosa@gmail.com>
2022-10-03 22:18:19 +0200

    Añadido ejercicio 3

--- ejercicios/JS-Functions/ejercicio-3.js
+++ ejercicios/JS-Functions/ejercicio-3.js
@@ -7,5 +7,13 @@
 const numbers = [1, 2, 3, 5, 45, 37, 58];
 
 function sumAll(param) {
-  // insert code
+  let result = 0;
+  
+  for (let i = 0; i < param.length; i++){
+    result += param[i];
+  };
+  
+  return result;
 }
+
+console.log(sumAll(numbers))
