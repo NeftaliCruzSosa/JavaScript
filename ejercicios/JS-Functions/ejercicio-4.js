69f65088c64108b83ec17872c2f454dd428d032c
neftalicruzsosa <neftalicruzsosa@gmail.com>
2022-10-03 22:23:09 +0200

    Añadido ejercicio 4

--- ejercicios/JS-Functions/ejercicio-4.js
+++ ejercicios/JS-Functions/ejercicio-4.js
@@ -3,6 +3,15 @@
 // Puedes usar este array para probar tu función:
 
 const numbers = [12, 21, 38, 5, 45, 37, 6];
+
 function average(param) {
-  // insert code
+  let sum = 0;
+  
+  for (let i = 0; i < param.length; i++){
+    sum += param[i];
+  };
+  
+  return sum / param.length;
 }
+
+console.log(average(numbers));
