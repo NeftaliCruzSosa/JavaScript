1cd3ebabe1afce77d09b47e8e448a28212277765
neftalicruzsosa <neftalicruzsosa@gmail.com>
2022-10-03 18:19:20 +0200

    Creada carpeta para ejercicios

--- sesion-1/index.js
+++ sesion-1/index.js
@@ -1,1 +1,13 @@
-console.log('Esto es una prueba de impresión')
+// console.log('Esto es una prueba de impresión')
+
+// const testDiv = document.getElementById('test')
+// console.log('DIV CAPTURADO', testDiv)
+
+// alert('Hola, esto es un alert')
+
+// console.log('Hola');
+// console.log('Adios');
+
+var saludo = prompt('¿Cómo quieres saludar?');
+
+console.log('El usuario a saludado con :' + saludo)
