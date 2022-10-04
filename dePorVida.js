67b99d61257866ea9d1c0e974365e893dd362db4
neftalicruzsosa <neftalicruzsosa@gmail.com>
2022-10-04 13:14:34 +0200

    Optimizado ejercicio dePorVida.js

--- dePorVida.js
+++ dePorVida.js
@@ -3,13 +3,20 @@ let dailyConsumption = 1.5;
 
 function calc() {
   let totalDays = coffeeBags / dailyConsumption;
-    
-  years = Math.floor(totalDays/365)
-  totalDays -= years*365
-  months = Math.floor(totalDays/30)
-  totalDays -= months*30
-  
-  return "Me van a durar " + years + " año(s), " + months + " mes(es) y " + Math.floor(totalDays) + " dia(s)"
+
+  years = Math.floor(totalDays / 365);
+  months = Math.floor((totalDays % 365) / 30);
+  days = Math.floor((totalDays % 365) % 30);
+
+  return (
+    "Me van a durar " +
+    years +
+    " año(s), " +
+    months +
+    " mes(es) y " +
+    days +
+    " dia(s)"
+  );
 }
 
-console.log(calc())
+console.log(calc());
