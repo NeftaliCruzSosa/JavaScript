c6111789e44653f20aef719af82026caa582d45e
neftalicruzsosa <neftalicruzsosa@gmail.com>
2022-10-06 11:51:37 +0200

    Añadido ejercicio-4, revisando enunciado ejercicio-3

---
+++ ejercicios/JS-Review-Basics/ejercicio-4.js
@@ -0,0 +1,23 @@
+// Crea una función llamada `findArrayIndex` que reciba como parametros un array de textos
+// y un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste
+// como parametro.
+
+// Haz varios ejemplos y compruebalos.
+
+// Sugerencia de función:
+
+// function findArrayIndex(array, text) {}
+
+// Ej array:
+
+const animals = ["Caracol", "Mosquito", "Salamandra", "Ajolote"];
+
+function findArrayIndex(array, text) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === text) {
+            return i
+        }
+    }
+}
+
+console.log(findArrayIndex(animals, "Salamandra"))
