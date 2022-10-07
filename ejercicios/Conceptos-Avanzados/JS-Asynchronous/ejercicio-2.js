2296ae0f0011580b8a1e7eeeef704030dd27da56
neftalicruzsosa <neftalicruzsosa@gmail.com>
2022-10-07 19:27:20 +0200

    Modificado sistema de carpetas y añadidos archivos de ejercicios del apartado conceptos avanzados 'Solo tienen el enunciado'

---
+++ ejercicios/Conceptos-Avanzados/JS-Asynchronous/ejercicio-2.js
@@ -0,0 +1,30 @@
+// 2.1 Convierte la siguiente promesa para esperar a ejecutar el console usando 
+// async-await.
+// const runTimeOut = () => {
+//     const promise = new Promise((resolve) => {
+//         setTimeout(function () {
+//             resolve();
+//         }, 2000);
+//     })
+
+//     promise.then(() => {console.log('Time out!')})
+// };
+
+// runTimeOut();
+
+// 2.2 Convierte la siguiente función con un fetch utilizando async-await. 
+// Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
+// function getCharacters () {
+//     fetch('https://rickandmortyapi.com/api/character').then(res => res.json()).then(characters => console.log(characters));
+// }
+
+// getCharacters();
+
+// 2.3 En base al ejercicio anterior. Crea dinamicamente un elemento  por cada petición 
+// a la api que diga...'El nombre X tiene un Y porciento de ser de Z' etc etc.
+// EJ: El nombre Pepe tiene un 22 porciento de ser de ET y un 6 porciento de ser 
+// de MZ.
+
+// 2.4 En base al ejercicio anterior, crea un botón con el texto 'X' para cada uno 
+// de los p que hayas insertado y que si el usuario hace click en este botón 
+// eliminemos el parrafo asociado.
