970c965cac44510f73da61dbaa6e2db7e0528a0b
neftalicruzsosa <neftalicruzsosa@gmail.com>
2022-10-12 19:48:39 +0200

    Añadido ejercicio JS-Events

--- ejercicios/Conceptos-Avanzados/JS-Events/ejercicio-1.js
+++ ejercicios/Conceptos-Avanzados/JS-Events/ejercicio-1.js
@@ -1,24 +1,10 @@
-// Dado el siguiente HTML:
-
-
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Document</title>
-// </head>
-// <body>
-// 	<input class="click" type="text"/>
-// 	<input class="focus" type="text"/>
-// 	<input class="value" type="text"/>
-// </body>
-// </html>
-
-
 // 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
 // evento click que ejecute un console log con la información del evento del click
-
+let button$$ = document.querySelector('#btnToClick')
+button$$.addEventListener('click', (event) => console.log(event))
 // 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
-
+let focus$$ = document.querySelector('.focus')
+focus$$.addEventListener('focus',() => (console.log(focus$$.value)))
 // 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
+let value$$ = document.querySelector('.value')
+value$$.addEventListener('input',() => (console.log(value$$.value)))
