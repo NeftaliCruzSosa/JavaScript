33cdbbd1dc3bf9280731883954b6fa146cedf955
neftalicruzsosa <neftalicruzsosa@gmail.com>
2022-10-14 10:43:52 +0200

    Añadido iteracion 3 y 4 del ejercicio 1

--- ejercicios/Conceptos-Avanzados/JS-Asynchronous/ejercicio-1.js
+++ ejercicios/Conceptos-Avanzados/JS-Asynchronous/ejercicio-1.js
@@ -6,18 +6,55 @@ fetch("https://api.agify.io?name=michael")
   .then((person) => person.json())
   .then((person) => console.log(person));
 
-// 2.1 Dado el siguiente javascript y html. Añade la funcionalidad necesaria usando
+// 1.2 Dado el siguiente javascript y html. Añade la funcionalidad necesaria usando
 // fetch() para hacer una consulta a la api cuando se haga click en el botón,
 // pasando como parametro de la api, el valor del input.
 // const baseUrl = 'https://api.nationalize.io';
 
-const input$$ = document.querySelector("input")
+const input$$ = document.querySelector("input");
 const button$$ = document.querySelector("button");
 
-const data = (name) =>{
-    fetch('https://api.nationalize.io?name=' + name)
-    .then((person) => person.json())
-    .then((person) => console.log(person))
-}
+// const consoleData = (name) =>{
+//     fetch('https://api.nationalize.io?name=' + name)
+//     .then((person) => person.json())
+//     .then((person) => console.log(person))
+// }
 
-button$$.addEventListener('click', () => data(input$$.value))
+// button$$.addEventListener('click', () => consoleData(input$$.value))
+
+// 1.3 En base al ejercicio anterior. Crea dinamicamente un elemento  por cada petición
+// a la api que diga...'El nombre X tiene un Y porciento de ser de Z' etc etc.
+// EJ: El nombre Pepe tiene un 22 porciento de ser de ET y un 6 porciento de ser
+// de MZ.
+
+const pData = (name) => {
+  const p$$ = document.createElement("p");
+  p$$.textContent = `El nombre ${input$$.value} `;
+
+  fetch("https://api.nationalize.io?name=" + name)
+    .then((person) => person.json())
+    .then((person) => {
+
+      for (const country of person.country) {
+        countryP$$ = document.createElement("p");       
+        countryP$$.textContent += `tiene un ${(country.probability * 100).toFixed(2)} porciento de ser de ${
+            country.country_id
+          }`;
+
+          p$$.appendChild(countryP$$)
+
+      }
+    });
+    deleteButton$$ = document.createElement("button");
+          deleteButton$$.textContent = ("X")
+          deleteButton$$.addEventListener('click', () => p$$.remove())
+          p$$.appendChild(deleteButton$$)
+    
+    document.body.appendChild(p$$);
+};
+
+button$$.addEventListener("click", () => pData(input$$.value));
+
+// 1.4 En base al ejercicio anterior, crea un botón con el texto 'X' para cada uno
+// de los p que hayas insertado y que si el usuario hace click en este botón
+// eliminemos el parrafo asociado.
--- ejercicios/Conceptos-Avanzados/JS-Asynchronous/ejercicio-2.js
+++ ejercicios/Conceptos-Avanzados/JS-Asynchronous/ejercicio-2.js
@@ -19,12 +19,3 @@
 // }
 
 // getCharacters();
-
-// 2.3 En base al ejercicio anterior. Crea dinamicamente un elemento  por cada petición 
-// a la api que diga...'El nombre X tiene un Y porciento de ser de Z' etc etc.
-// EJ: El nombre Pepe tiene un 22 porciento de ser de ET y un 6 porciento de ser 
-// de MZ.
-
-// 2.4 En base al ejercicio anterior, crea un botón con el texto 'X' para cada uno 
-// de los p que hayas insertado y que si el usuario hace click en este botón 
-// eliminemos el parrafo asociado.
